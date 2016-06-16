f498677f3dd61aafb4c82f8beaeba6b0cbbe20fd
indes <wanglicsly@gmail.com>
2016-06-16 21:17:26 +0800

    模板初步整合

--- controllers/default.go
+++ controllers/default.go
@@ -11,13 +11,27 @@ type RtimeController struct {
 	beego.Controller
 }
 
+type HistorydataController struct {
+	beego.Controller
+}
+type DataController struct {
+	beego.Controller
+}
+
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplNames = "index.tpl"
+	c.TplName = "index.tpl"
 }
 
-
 func (c *RtimeController) Get() {
-	c.Ctx.WriteString("hello world")
+	c.TplName = "index.html"
+}
+
+func (c *HistorydataController) Get() {
+	c.TplName = "historydata.html"
+}
+func (c *DataController) Get() {
+	//c.Ctx.ResponseWriter.Header("Content-type: text/javascript;charset=UTF-8")
+	c.TplName = "data.tpl"
 }
--- routers/router.go
+++ routers/router.go
@@ -2,11 +2,13 @@ package routers
 
 import (
 	"wmoniter_serv/controllers"
-
 	"github.com/astaxie/beego"
 )
 
 func init() {
-	beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.RtimeController{})
 	beego.Router("/rtime", &controllers.RtimeController{})
+	beego.Router("/historydata", &controllers.HistorydataController{})
+	beego.Router("/data", &controllers.DataController{})
+
 }
